3558eabc831214af75bc74476f539818f65734a3
ByGustavoo <132701636+ByGustavoo@users.noreply.github.com>
2026-05-07 22:19:24 -0300

    ADJ-1
    - Commit Inicial

---
+++ settings.gradle.kts
@@ -0,0 +1,1 @@
+rootProject.name = "FASE2-12ADJT"
---
+++ src/main/java/br/com/fiap/api/Fase212AdjtApplication.java
@@ -0,0 +1,13 @@
+package br.com.fiap.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Fase212AdjtApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Fase212AdjtApplication.class, args);
+	}
+
+}
